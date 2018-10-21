704928278f4c8092f0be81b8db0ac70e904ace01
andrewhoang <a.hoang95@gmail.com>
2018-10-20 21:14:08 -0400

    configure port

--- server.js
+++ server.js
@@ -86,11 +86,11 @@ app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, 'localhost', err => {
+app.listen(process.env.PORT || 3000, err => {
 	if (err) {
 		console.log(err);
 		return;
 	}
 
-	console.log('Listening at http://localhost:3000');
+	console.log(`Listening at http://localhost:${process.env.PORT || 3000}`);
 });
