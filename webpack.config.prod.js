d90706ff2c81967de22ad7fa7eba4a32f2643ba7
andrewhoang <a.hoang95@gmail.com>
2018-10-26 20:50:25 -0400

    remove uglifyjs

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
@@ -18,14 +17,7 @@ module.exports = {
 		publicPath: '/static/',
 	},
 	optimization: {
-		minimizer: [
-			new UglifyJsPlugin({
-				cache: true,
-				parallel: true,
-				sourceMap: true, // set to true if you want JS source maps
-			}),
-			new OptimizeCSSAssetsPlugin({}),
-		],
+		minimizer: [new OptimizeCSSAssetsPlugin({})],
 	},
 	plugins: [
 		new MiniCssExtractPlugin({
