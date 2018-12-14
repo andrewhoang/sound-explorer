7ebc24cdc4c0b6ad07b1eacf5844c8453370e8ca
andrewhoang <a.hoang95@gmail.com>
2018-12-14 18:52:14 -0500

    mobile styling

--- src/client/components/playlists/MobileTrackList.js
+++ src/client/components/playlists/MobileTrackList.js
@@ -4,7 +4,7 @@ import 'react-table/react-table.css';
 
 import ReactTable from 'react-table';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faPause, faTrash, faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faPlay, faPause, faTimes, faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 import { Row, Col } from 'react-bootstrap';
 
@@ -12,28 +12,29 @@ const MobileTrackList = ({ playlist, playing, track, onClickPlay, onClickPause, 
 	return playlist.map(item => {
 		const isPlaying = playing && track === item.uri;
 		return (
-			<Row
-				className="track-list mobile"
-				onClick={() =>
-					!playing
-						? onClickPlay(item.uri)
-						: playing && track === item.uri
-						? onClickPause(item.uri)
-						: playing && track !== item.uri && onClickPlay(item.uri)
-				}
-			>
+			<Row className="track-list mobile">
 				<Col
-					md={12}
+					xs={10}
 					style={{
 						color: isPlaying ? '#1db954' : '#fff',
 						fontWeight: isPlaying ? 700 : 400,
 					}}
+					onClick={() =>
+						!playing
+							? onClickPlay(item.uri)
+							: playing && track === item.uri
+							? onClickPause(item.uri)
+							: playing && track !== item.uri && onClickPlay(item.uri)
+					}
 				>
 					<strong>{item.name}</strong>
 					<p>
 						{item.artists.map((artist, i) => (i == 0 ? `${artist.name}` : ` ${artist.name}`)).toString()} •{' '}
 						{item.album.name}
 					</p>
+				</Col>
+				<Col xs={2}>
+					<FontAwesomeIcon style={{ float: 'right' }} icon={faTimes} onClick={() => onClickRemove(item.id)} />
 				</Col>
 			</Row>
 		);
